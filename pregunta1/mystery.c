eac5339acf2797446834d5e3a81891ae45352ac0
Bruno <omarjm0910@gmail.com>
2022-03-28 10:54:17 -0600

    prueba

--- pregunta1/mystery.c
+++ pregunta1/mystery.c
@@ -7,7 +7,7 @@ int main() {
     for(i=0; i<10; i++) {
         x[i] = i;
     }
-
+    printf(x)
     // ¿Por qué se imprimen alternados con 0 los numeros asignados en el for anterior?
     for(i=0; i<10; i++) {
         printf("%d\n", *I2S(x+i));

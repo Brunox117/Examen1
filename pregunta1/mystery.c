6f77ee9ab273659ce14b0478b183568c693b394d
Bruno <omarjm0910@gmail.com>
2022-03-28 11:47:18 -0600

    0

--- pregunta1/mystery.c
+++ pregunta1/mystery.c
@@ -8,11 +8,12 @@ int main() {
         x[i] = i;
     }
     // ¿Por qué se imprimen alternados con 0 los numeros asignados en el for anterior?
-    
-    for(i=0; i<10; i++) {
-        printf("el valor de i es %d",i);
-        printf("x+i %d",x+i)
+    //Esto se debe a los bytes que cause que imprima uno seguido de otro si aumentamos
+    //el tope imprime los 9 numeros y sigue imprimiendo el 0 por los bytes al usar short * y * a
+    //antes de llamar a la macro
+    for(i=0; i<20; i++) {
         printf("%d\n", *I2S(x+i));
+        
     }
     
 }
